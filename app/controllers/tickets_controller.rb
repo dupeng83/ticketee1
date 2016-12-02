a305f44d59c0c0185a6b184ffa92698b1c0d7400
dupeng <dupeng83@outlook.com>
2016-12-02 14:41:59 +0800

    Implement editing and deleting tickets feature 实现了edit和delete ticket

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,5 +1,6 @@
 class TicketsController < ApplicationController
   before_action :set_project
+  before_action :set_ticket, only: [:show, :edit, :update, :destroy]
 
   def new
     @ticket = @project.tickets.build
@@ -19,14 +20,41 @@ class TicketsController < ApplicationController
   end
 
   def show
-    # debugger
-    @ticket = @project.tickets.find( params[:id] ) 
+  end
+
+  def edit
+  end
+
+  def update
+    # @ticket = @project.tickets.build( ticket_params )
+    # @ticket = @project.tickets.find( params[:id] )
+
+    if @ticket.update(ticket_params)
+      flash[:notice] = "Ticket has been updated."
+      redirect_to [@project, @ticket]
+    else
+      flash.now[:alert] = "Ticket has not been updated."
+      render "edit"
+    end
+  end
+
+  def destroy
+    # @ticket = @project.tickets.find( params[:id] )
+
+    @ticket.destroy
+    flash[:notice] = "Ticket has been deleted."
+
+    redirect_to @project
   end
 
   private
 
     def set_project
       @project = Project.find( params[:project_id] )
+    end
+
+    def set_ticket
+      @ticket = @project.tickets.find(params[:id])
     end
 
     def ticket_params
