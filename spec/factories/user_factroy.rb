4130afbdd30e1851b6dfa98b279635deff76e0e1
dupeng <dupeng83@outlook.com>
2016-12-05 16:49:44 +0800

    Added admin flag to User model, and seeded the first admin user 新

--- spec/factories/user_factroy.rb
+++ spec/factories/user_factroy.rb
@@ -1,7 +1,10 @@
 FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "test#{n}@example.com" }
-    # email "dup@sina.com.cn"
     password "password"
+
+    trait :admin do
+      admin true
+    end
   end
 end
