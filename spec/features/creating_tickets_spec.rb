3a8d69009688d40ae546f64dcdec7fafe51cd661
dupeng <dupeng83@outlook.com>
2017-02-03 11:49:37 +0800

    Disable file uploading and comment out the spec scenario

--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -60,18 +60,18 @@ RSpec.feature "Users can create new tickets" do
   #   end
   # end
 
-  scenario "persisting file uploads across form displays" do
-    attach_file "File #1", "spec/fixtures/speed.txt"
-    click_button "Create Ticket"
+  # scenario "persisting file uploads across form displays" do
+  #   attach_file "File #1", "spec/fixtures/speed.txt"
+  #   click_button "Create Ticket"
 
-    fill_in "Name", with: "Add documentation for blink tag"
-    fill_in "Description", with: "The blink tag has a speed attribute"
-    click_button "Create Ticket"
+  #   fill_in "Name", with: "Add documentation for blink tag"
+  #   fill_in "Description", with: "The blink tag has a speed attribute"
+  #   click_button "Create Ticket"
 
-    within("#ticket .attachments") do
-      expect(page).to have_content "speed.txt"
-    end
-  end
+  #   within("#ticket .attachments") do
+  #     expect(page).to have_content "speed.txt"
+  #   end
+  # end
 
   scenario "with associated tags" do
     fill_in "Name", with: "Non-standards compliance"
